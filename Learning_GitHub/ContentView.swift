ae7b3bd2ebaf1ada4c63860ff56cf9b123e85689
Martin-Matzer <martin.matzer@gmx.net>
2021-09-15 23:34:48 +0200

    Hallo, world! Here is my first commit.

--- Learning_GitHub/ContentView.swift
+++ Learning_GitHub/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        Text("Hello, world! Here is my firt commit.")
             .padding()
     }
 }
