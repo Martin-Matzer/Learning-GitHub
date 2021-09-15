b0a57f15d6787d4799c004d16399fa25d7f6be8b
Martin-Matzer <martin.matzer@gmx.net>
2021-09-15 23:30:15 +0200

    Initial Commit

---
+++ Learning_GitHub/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Learning_GitHub
+//
+//  Created by Martin Matzer on 15.09.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Learning_GitHub/Learning_GitHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learning_GitHubApp.swift
+//  Learning_GitHub
+//
+//  Created by Martin Matzer on 15.09.21.
+//
+
+import SwiftUI
+
+@main
+struct Learning_GitHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
