692ad0305154716a1afedf4b1bfb5bbf21e82da3
Martin-Matzer <90801896+Martin-Matzer@users.noreply.github.com>
2021-09-21 22:25:13 +0200

    Update ContentView to What's up

--- Learning_GitHub/ContentView.swift
+++ Learning_GitHub/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world! Here is my firt commit.")
+        Text("What's up, world! Here is my firt commit.")
             .padding()
     }
 }
